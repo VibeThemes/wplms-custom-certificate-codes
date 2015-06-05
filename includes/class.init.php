cfe1536edcef2b3a99131eb7701d28ede8fc8287
megha <megha@99fusion.com>
2015-06-05 12:51:35 +0530

    garbage value

--- includes/class.init.php
+++ includes/class.init.php
@@ -1,3 +1,9 @@
 <?php
 
-
+//add_action('init','add_garbage_data');
+function add_garbage_data(){
+echo'@@@';
+	add_option('2060-1139-1','ABC1');
+	add_option('2060-1260-1','ABC2');
+	add_option('wplms_certificate_codes',array('2060-1139-1','2060-1260-1'));
+}
